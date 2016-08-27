bf01cffa9c32f48e53cd66046e13d93b5597e33c
Xapac <xapac@list.ru>
2016-08-27 10:57:26 +0300

    реализован  #9

--- js/Box2D.js
+++ js/Box2D.js
@@ -9,7 +9,7 @@ function Box2D(v1, v2){
     /*
     Позиция бокса
     */
-    this.position = null;
+    //this.position = null;
 
     /*
     Верхняя левая координата
@@ -25,6 +25,35 @@ function Box2D(v1, v2){
         this.leftUp = lup;
         this.rightDown = rdown;
     }
+
+    this.getCenterPosition = function(){
+        var t = this.leftUp.multipy(this.rightDown);
+        t.x = t.x/2;
+        t.y = t.y/2;
+        return t; 
+    }
+
+    this.сollisionToBox2D = function(box){
+        
+        //Получаем центры 
+        var pos1 = this.getCenterPosition();
+        var pos2 = box.getCenterPosition();
+
+        //Получаем расстояния от центров
+        var tX = Math.abs(pos1.x - pos2.x);
+        var tY = Math.abs(pos1.y - pos2.y);
+
+        //Получаем сумму половины длин сторон
+        var dt = this.leftUp.subtract(this.rightDown);
+        var db = box.leftUp.subtract(box.rightDown);
+
+        if(
+            (tX > (dt.x + db.x)) &&
+            (tY > (dt.y + db.y))
+            ) return true;
+        return false;
+    }
+
 
     this.draw = function(context){
         
